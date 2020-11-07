ca8468a0738c8ae0c80ebc5dafdb682455fc55a5
AlexM05 <47923859+AlexM05@users.noreply.github.com>
2020-11-06 18:28:13 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+with open('wordlist.txt', 'r') as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+
+allowed_errors = 6
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+    done = True
+
+    guess = input(f"Allowed Errors Left {allowed_errors}, Next Guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+
+if done:
+    print(f"You found the word! It was {word}!")
+else:
+    print(f"Game Over! The word was {word}!")
